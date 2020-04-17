4d70866ed3f985d59e6cc3c24beb4c310b39c98d
Ivan Gerasimenko <1315.ig@gmail.com>
2020-04-17 23:35:48 +0300

    fix2 functions parida.php

--- web/app/themes/pretolani/resources/functions/parida.php
+++ web/app/themes/pretolani/resources/functions/parida.php
@@ -1,10 +1,6 @@
 <?php
 
-namespace App;
-
-use Roots\Sage\Container;
-
 add_action( 'wp_enqueue_scripts', 'wp_enqueue_scripts_parida' );
 function wp_enqueue_scripts_parida() {
-    wp_enqueue_style('sage/parida.css', asset_path('styles/parida.css'), false, null);
+    wp_enqueue_style('parida', get_template_directory_uri() . 'styles/parida.css');
 }
