8cdac6d9e6780bcbf933c30d26968df9eb23b3e7
Ivan Gerasimenko <1315.ig@gmail.com>
2020-04-20 16:07:34 +0300

    2 20.04.2020

--- web/app/themes/pretolani/resources/functions/parida.php
+++ web/app/themes/pretolani/resources/functions/parida.php
@@ -1,5 +1,5 @@
 <?php
 
 add_action('wp_enqueue_scripts', function () {
-    wp_enqueue_style('parida', get_template_directory_uri().'styles/parida.css', false, null);
+    wp_enqueue_style('parida', get_template_directory_uri().'/assets/styles/parida.css', false, null);
 }, 200);
