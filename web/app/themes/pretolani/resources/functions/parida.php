aac7029aacf4e0936a0d2a2a9c2d8598e1dc9d05
Ivan Gerasimenko <1315.ig@gmail.com>
2020-04-20 17:50:05 +0300

    3 20.04.2020

--- web/app/themes/pretolani/resources/functions/parida.php
+++ web/app/themes/pretolani/resources/functions/parida.php
@@ -1,5 +1,16 @@
 <?php
 
 add_action('wp_enqueue_scripts', function () {
-    wp_enqueue_style('parida', get_template_directory_uri().'/assets/styles/parida.css', false, null);
+    wp_enqueue_style('styles-animate', get_template_directory_uri().'/assets/styles/animate.css', false, null);
+    wp_enqueue_style('styles-fullpage-min-parida', get_template_directory_uri().'/assets/styles/fullpage.min.css', false, null);
+    wp_enqueue_style('styles-media', get_template_directory_uri().'/assets/styles/media.css', false, null);
+    wp_enqueue_style('styles-swiper-min', get_template_directory_uri().'/assets/styles/swiper.min.css', false, null);
+
+    wp_enqueue_script( 'script-fullpage-extensions-min', get_template_directory_uri() . '/assets/scripts/fullpage.extensions.min.js' );
+    wp_enqueue_script( 'script-fullpage', get_template_directory_uri() . '/assets/scripts/fullpage.js' );
+    wp_enqueue_script( 'script-gsap-latest-beta-min', get_template_directory_uri() . '/assets/scripts/gsap-latest-beta.min.js' );
+    wp_enqueue_script( 'script-script', get_template_directory_uri() . '/assets/scripts/script.js' );
+    wp_enqueue_script( 'script-SplitText3-min', get_template_directory_uri() . '/assets/scripts/SplitText3.min.js' );
+    wp_enqueue_script( 'script-swiper-min', get_template_directory_uri() . '/assets/scripts/swiper.min.js' );
+    wp_enqueue_script( 'script-wow', get_template_directory_uri() . '/assets/scripts/wow.js' );
 }, 200);
