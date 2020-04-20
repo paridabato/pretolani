b9fbd463159d546c1df3257217b195dc0e8f362c
Ivan Gerasimenko <1315.ig@gmail.com>
2020-04-20 22:32:34 +0300

    14 20.04.2020

--- web/app/themes/pretolani/resources/views/partials/menu.blade.php
+++ web/app/themes/pretolani/resources/views/partials/menu.blade.php
@@ -1,23 +1,23 @@
-<?php global $tp; ?>
+@php global $tp; @endphp
 <div class="menu fw">
   <div class="menu__wrapper wrapper">
     <div class="menu__links">
       <div class="menu__block menu__block_images">
-        <img src="<?php echo $tp; ?>/img/menu-image-demo.png"/>
-        <img src="<?php echo $tp; ?>/img/menu-image-demo.png"/>
-        <img src="<?php echo $tp; ?>/img/menu-image-demo.png"/>
-        <img src="<?php echo $tp; ?>/img/menu-image-demo.png"/>
-        <img src="<?php echo $tp; ?>/img/menu-image-demo.png"/>
+        <img src="@php echo $tp; @endphp/img/menu-image-demo.png"/>
+        <img src="@php echo $tp; @endphp/img/menu-image-demo.png"/>
+        <img src="@php echo $tp; @endphp/img/menu-image-demo.png"/>
+        <img src="@php echo $tp; @endphp/img/menu-image-demo.png"/>
+        <img src="@php echo $tp; @endphp/img/menu-image-demo.png"/>
       </div>
       <div class="menu__block menu__block_nav">
-        <?php
+        @php
         wp_nav_menu(array(
           'container' => '',
           'theme_location' => 'main_menu',
           'menu_class' => 'menu__nav',
           'depth' => 0,
         ));
-        ?>
+        @endphp
       </div>
       <div class="menu__block"></div>
     </div>
