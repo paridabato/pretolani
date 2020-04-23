664f1ac7e5f0f12afe7e6debf279b800d414d935
Ivan Herasymenko <1315.ig@gmail.com>
2020-04-24 01:00:05 +0300

    8 24.04.2020

--- web/app/themes/pretolani/resources/views/partials/block-main-section.blade.php
+++ web/app/themes/pretolani/resources/views/partials/block-main-section.blade.php
@@ -7,13 +7,13 @@
         <div class="flex-row flex-row_relative">
             <div class="flex-block">
             	@if(!empty($mainblock['title']))
-                	<h1 class="section-title sep-letters">{{ $mainblock['title'] }}</h1>
+                	<h1 class="section-title sep-letters">{!! $mainblock['title'] !!}</h1>
                 @endif
             </div>
             <div class="flex-block flex-block_padd">
                 <div class="flex-block__content">
                 	@if(!empty($mainblock['text']))
-                    	<div class="quote-text description sep-lines">{{ $mainblock['text'] }}</div>
+                    	<div class="quote-text description sep-lines">{!! $mainblock['text'] !!}</div>
                     @endif
                 </div>
             </div>
@@ -22,7 +22,7 @@
     @if(!empty($mainblock['image']))
         <div class="fw-image-wrapper">
             <div class="fw-image fw-image_halfslide fw">
-                <img src="{{ $mainblock['image'] }}">
+                <img src="{!! $mainblock['image'] !!}">
             </div>
         </div>
     @endif
