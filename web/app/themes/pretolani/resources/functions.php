f707ae7dc4d22fde9131475346e15820cb939dde
Ivan Herasymenko <1315.ig@gmail.com>
2020-04-24 00:15:50 +0300

    3 24.04.2020

--- web/app/themes/pretolani/resources/functions.php
+++ web/app/themes/pretolani/resources/functions.php
@@ -97,3 +97,171 @@ Container::getInstance()
 foreach (glob(__DIR__ .'/functions/*.php') as $filename) {
     include $filename;
 }
+
+
+/* REMOVE EMOJI START */
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('admin_print_scripts', 'print_emoji_detection_script');
+remove_action('wp_print_styles', 'print_emoji_styles');
+remove_action('admin_print_styles', 'print_emoji_styles');
+/* REMOVE EMOJI END */
+
+remove_filter('the_content', 'wpautop');
+
+add_filter('excerpt_length', function () {
+    return 50;
+});
+
+/* REMOVE GUTENBERG STYLES START */
+
+function smartwp_remove_wp_block_library_css()
+{
+    wp_dequeue_style('wp-block-library');
+    wp_dequeue_style('wp-block-library-theme');
+}
+
+add_action('wp_enqueue_scripts', 'smartwp_remove_wp_block_library_css');
+
+/* REMOVE GUTENBERG STYLES END */
+
+/* REMOVE ATTRS START */
+add_action('wp_loaded', 'prefix_output_buffer_start');
+function prefix_output_buffer_start()
+{
+    ob_start("prefix_output_callback");
+}
+
+function prefix_output_callback($buffer)
+{
+    return preg_replace("%[ ]type=[\'\"]text\/(javascript|css)[\'\"]%", '', $buffer);
+}
+
+/* REMOVE ATTRS END */
+
+/* CF7 remove tags START */
+add_filter('wpcf7_autop_or_not', '__return_false');
+/* CF7 remove tags end */
+
+
+function load_js_css()
+{
+    wp_enqueue_style('fonts', get_template_directory_uri() . '/assets/fonts/stylesheet.css');
+    wp_enqueue_style('animate', get_template_directory_uri() . '/assets/styles/animate.css');
+    wp_enqueue_style('fullpage', get_template_directory_uri() . '/assets/styles/fullpage.min.css');
+    wp_enqueue_style('swiper', get_template_directory_uri() . '/assets/styles/swiper.min.css');
+
+    wp_enqueue_style('styles', get_stylesheet_uri());
+    wp_enqueue_style('media-styles', get_template_directory_uri() . '/assets/styles/media.css');
+
+
+    wp_enqueue_script('jquery-js', get_template_directory_uri() . '/assets/scripts/jquery-1.11.1.min.js', array(), null, false);
+    // wp_localize_script('jquery-js', 'ajaxurl', admin_url( 'admin-ajax.php' ) );
+
+    wp_enqueue_script('gsap', get_template_directory_uri() . '/assets/scripts/gsap-latest-beta.min.js', array('jquery'), null, false);
+    wp_enqueue_script('splittext', get_template_directory_uri() . '/assets/scripts/SplitText3.min.js', array('jquery'), null, false);
+    wp_enqueue_script('fullpage', get_template_directory_uri() . '/assets/scripts/fullpage.js', array('jquery'), null, false);
+    wp_enqueue_script('wow', get_template_directory_uri() . '/assets/scripts/wow.js', array('jquery'), null, false);
+    wp_enqueue_script('swiper', get_template_directory_uri() . '/assets/scripts/swiper.min.js', array('jquery'), null, false);
+    wp_enqueue_script('script', get_template_directory_uri() . '/assets/scripts/script.js', array('jquery'), null, false);
+}
+
+add_action('wp_enqueue_scripts', 'load_js_css');
+
+
+/* POST THUMBNAILS START */
+/*if (function_exists('add_theme_support')) {
+    add_theme_support('post-thumbnails');
+    set_post_thumbnail_size(412, 240);
+}*/
+
+/* POST THUMBNAILS END */
+
+
+add_action('after_setup_theme', function () {
+    register_nav_menus(array(
+        'main_menu' => 'Main menu',
+    ));
+});
+
+
+/* ACF theme options START */
+if (function_exists('acf_add_options_page')) {
+    acf_add_options_page(
+        array(
+            'page_title' => 'Theme settings',
+            'menu_title' => 'Theme settings',
+            'menu_slug' => 'theme-options',
+            'capability' => 'edit_posts',
+            'parent_slug' => '',
+            'position' => false,
+        )
+    );
+    acf_add_options_sub_page(
+        array(
+            'page_title' => 'Global',
+            'menu_title' => 'Global',
+            'menu_slug' => 'theme-options-global',
+            'capability' => 'edit_posts',
+            'parent_slug' => 'theme-options',
+            'position' => false,
+            'icon_url' => false,
+            'redirect' => false,
+        )
+    );
+}
+/* ACF theme options END */
+
+
+/* Fix 404 error for custom post type pages */
+
+flush_rewrite_rules(false);
+
+/* Thumbs START */
+/*add_image_size('social-icon', 32, 32, true);
+add_image_size('info-icon', 75, 75, true);
+add_image_size('expertise-icon', 40, 40, true);*/
+/* Thumbs END */
+
+
+/*
+
+function catName($cat_id) {
+    $cat_id = (int) $cat_id;
+    $category = get_category($cat_id);
+    return $category->name;
+}
+function catLink($cat_id) {
+    $category = get_the_category();
+    $category_link = get_category_link($cat_id);
+    return $category_link;
+}
+*/
+
+
+function language_selector_flags()
+{
+    $languages = icl_get_languages('skip_missing=0&orderby=code');
+    $lc = 1;
+    if (!empty($languages)) {
+        foreach ($languages as $l) {
+            if (!$l['active']) {
+                echo '<a class="langs__item" href="' . $l['url'] . '">';
+            } else {
+                echo '<a class="langs__item active">';
+            }
+            echo strtoupper($l['code']);
+            echo '</a>';
+            $lc++;
+        }
+    }
+}
+
+function gv($var) {
+    $gv = array();
+    $gv['tp'] = get_template_directory_uri();
+    $gv['imgp'] = get_template_directory_uri() . '/assets/images';
+    $gv['logo'] = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 346.2 47.97"><defs><style>.cls-1{fill:#1d1d1b;}</style></defs><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><g id="Logo"><path class="cls-1" d="M0,0V48H25.06L48.91,24.13V0ZM43.93,24v1.19H24V43.06H4.92V5h39Z"/><polygon class="cls-1" points="139.08 9.99 160.1 9.99 160.1 16.1 145.03 16.1 145.03 21.09 158.19 21.09 158.19 25.93 144.95 25.93 144.95 32.03 160.1 32.03 160.1 37.98 139.08 37.98 139.08 9.99"/><polygon class="cls-1" points="168.09 9.99 191.1 9.99 191.1 16.1 183.01 16.1 183.01 37.98 177.15 37.98 177.15 16.18 168.09 16.18 168.09 9.99"/><path class="cls-1" d="M213.52,9.82A14.17,14.17,0,1,0,227.68,24,14.16,14.16,0,0,0,213.52,9.82Zm0,22.27a8.1,8.1,0,1,1,8.1-8.1A8.11,8.11,0,0,1,213.52,32.09Z"/><polygon class="cls-1" points="238.89 9.99 238.89 37.98 258.24 37.98 258.24 32.12 245.02 32.12 245.02 9.99 238.89 9.99"/><polygon class="cls-1" points="304.33 9.99 310.36 9.99 322.2 25.24 322.2 9.99 329.07 9.99 329.07 37.98 323.37 37.98 311.42 22.05 311.31 37.98 304.33 37.98 304.33 9.99"/><polygon class="cls-1" points="340.18 9.99 346.2 9.99 346.2 37.98 340.18 38.15 340.18 9.99"/><path class="cls-1" d="M88.89,10H74V38h6V29.1h8.88s7-.79,7-9A10,10,0,0,0,88.89,10ZM86.27,23.07H80V16.15h6.23C91.93,20.06,86.27,23.07,86.27,23.07Z"/><path class="cls-1" d="M122.83,27.28s5.15.87,5.15-8.41S121,10,121,10H105.15V38h7v-9h3.81l6,9h7V36.24Zm-4.73-4.21h-5.92V16.15h5.92C125.76,19.85,118.1,23.07,118.1,23.07Z"/><path class="cls-1" d="M283.51,10h-6.24L266,38l6.23.17,3-7.09h10.79L288.69,38H295Zm-6.4,15.94c.11-.21,3.38-8,3.38-8l3.23,8Z"/></g></g></g></svg>';
+    return $gv[$var];
+}
+
+//wp_set_password( '54321', 2);
