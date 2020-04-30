4bd1a8e1d6ae9ee74b8c47a534e4569f2c0145dc
Ivan Gerasimenko <1315.ig@gmail.com>
2020-04-29 20:26:34 -0700

    js/css

--- web/app/themes/pretolani/resources/assets/scripts/routes/common.js
+++ web/app/themes/pretolani/resources/assets/scripts/routes/common.js
@@ -1,14 +1,15 @@
 import gdpr from '../components/gdpr';
-import sign148 from '../util/148';
+// import sign148 from '../util/148';
 import lazyload from '../util/lazyload';
-// import script from '../script';
+import script from '../script';
 
 export default {
   init() {
     // JavaScript to be fired on all pages
     gdpr();
-    sign148.init();
+    // sign148.init();
     lazyload();
+    script();
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
